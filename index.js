0b74d35b5e59ad3b0884a3bee0cb7150811932bd
jainshubham785 <jainshubham785@gmail.com>
2021-01-21 23:35:28 +0530

    version 1.2

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 let hours = document.getElementById("hours");
 let minutes = document.getElementById("minutes");
 let seconds = document.getElementById("seconds");
+let miliSeconds = document.getElementById("miliSeconds");
 let startBtn = document.getElementById("start");
 let pauseBtn = document.getElementById("pause");
 let stopBtn = document.getElementById("stop");
+
+let curr_second = 0;
+let curr_minute = 0;
+let curr_hour = 0;
+let curr_miliSec = 0;
 
 function start() {
     startBtn.disabled = true;
     pauseBtn.disabled = false;
     stopBtn.disabled = false;
-    intervalManager(true, incrementTimer, 1000);
+    curr_second = seconds.textContent;
+    curr_minute = minutes.textContent;
+    curr_hour = hours.textContent;
+    curr_miliSec = miliSeconds.textContent;
+    intervalManager(true, incrementTimer, 10);
 }
 
 var intervalId = null;
@@ -23,24 +33,37 @@ function intervalManager(flag, incrementTimer, time) {
 
 
 function incrementTimer() {
-    if (seconds.textContent == "60") {
-        seconds.textContent = "00";
-        minutes.textContent = Number(minutes.textContent) + 1;
-        if (Number(minutes.textContent) < 10) {
-            minutes.textContent = "0" + Number(minutes.textContent);
+    curr_miliSec++;
+    if (curr_miliSec == 99) {
+        curr_miliSec = 0;
+        curr_second++;
+        if (curr_second == 60) {
+            curr_second = 0;
+            curr_minute++;
+            if (curr_minute == 60) {
+                curr_minute = 0;
+                curr_hour++;
+                if (curr_hour < 10)
+                    hours.textContent = "0" + curr_hour;
+                else
+                    hours.textContent = curr_hour;
+            }
+            if (curr_minute < 10)
+                minutes.textContent = "0" + curr_minute;
+            else
+                minutes.textContent = curr_minute;
         }
+        if (curr_second < 10)
+            seconds.textContent = "0" + curr_second;
+        else
+            seconds.textContent = curr_second;
     }
-    if (minutes.textContent == "60") {
-        minutes.textContent = "00";
-        hours.textContent = Number(hours.textContent) + 1;
-        if (Number(hours.textContent) < 10) {
-            hours.textContent = "0" + Number(hours.textContent);
-        }
-    }
-    seconds.textContent = Number(seconds.textContent) + 1;
-    if (Number(seconds.textContent) < 10) {
-        seconds.textContent = "0" + Number(seconds.textContent);
-    }
+    if (curr_miliSec < 10)
+            miliSeconds.textContent = "0" + curr_miliSec;
+        else
+            miliSeconds.textContent = curr_miliSec;
+
+
 }
 
 
@@ -60,16 +83,18 @@ function stop() {
     pauseBtn.disabled = true;
     stopBtn.disabled = true;
     startBtn.disabled = false;
-    intervalManager(false);
+    pauseBtn.innerText = "pause";
     createTimeStamp();
     seconds.textContent = "00";
     minutes.textContent = "00";
     hours.textContent = "00";
+    miliSeconds.textContent = "00"
+    intervalManager(false);
 }
 
 function createTimeStamp() {
     let node = document.getElementById("timestamp");
     let node1 = document.createElement("li");
     node.appendChild(node1);
-    node1.textContent = hours.textContent + ":" + minutes.textContent + ":" + seconds.textContent;
+    node1.textContent = hours.textContent + ":" + minutes.textContent + ":" + seconds.textContent + ":" + miliSeconds.textContent;
 }
